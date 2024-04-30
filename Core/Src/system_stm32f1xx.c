41fb1e9d200ad95cef869bb935dc9adc1997491d
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-04-30 22:17:54 +0530

    Remove HAL support

--- Core/Src/system_stm32f1xx.c
+++ Core/Src/system_stm32f1xx.c
@@ -130,10 +130,7 @@
   * @{
   */
 
-  /* This variable is updated in three ways:
-      1) by calling CMSIS function SystemCoreClockUpdate()
-      2) by calling HAL API function HAL_RCC_GetHCLKFreq()
-      3) each time HAL_RCC_ClockConfig() is called to configure the system clock frequency 
+  /* This variable is updated by calling CMSIS function SystemCoreClockUpdate()
          Note: If you use this function to configure the system clock; then there
                is no need to call the 2 first functions listed above, since SystemCoreClock
                variable is updated automatically.
