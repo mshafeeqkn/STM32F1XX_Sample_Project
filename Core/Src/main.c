3b3a6dee1ff967e8fcb5008d8dca2b7331cb5dd7
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-05-09 17:10:47 +0530

    External interrupt EXTI0 from PB0

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,18 +23,17 @@
 #define TURN_OFF_LED()           turn_led_on(TURN_OFF)
 #define TOGGLE_LED()             turn_led_on(TURN_TOGGLE)
 
-
 typedef enum {
     TURN_OFF,
     TURN_ON,
     TURN_TOGGLE
 } LedState_t;
 
-void delay(uint32_t ms) {
-    // Simple delay function (not accurate, just for demonstration)
-    for (volatile uint32_t i = 0; i < ms * 1000; ++i) {
-        __NOP();  // No operation (compiler barrier)
-    }
+void turn_led_on(LedState_t state);
+
+void EXTI0_IRQHandler() {
+    EXTI->PR |= EXTI_PR_PR0;
+    TOGGLE_LED();
 }
 
 void turn_led_on(LedState_t state) {
@@ -47,22 +46,48 @@ void turn_led_on(LedState_t state) {
     }
 }
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
+void setup_exti0_interrupt() {
+    // Set PB0 as input
+    GPIOB->CRL &= ~GPIO_CRL_MODE0;
+
+    // Configure PB0 as input pull-down/pull-up
+    GPIOB->CRL &= ~GPIO_CRL_CNF0;
+    GPIOB->CRL |= GPIO_CRL_CNF0_1;
+
+    // Configure PB0 as pull-up
+    GPIOB->ODR |= GPIO_ODR_ODR0;
+
+    // Tie EXTI0 with PB0
+    AFIO->EXTICR[0] &= ~AFIO_EXTICR1_EXTI0;
+    AFIO->EXTICR[0] |= AFIO_EXTICR1_EXTI0_PB;
+
+    // Trigger on falling edge
+    EXTI->FTSR |= EXTI_FTSR_TR0;
+
+    // Mask EXTI0
+    EXTI->IMR |= EXTI_IMR_MR0;
+
+    // Clear the flag
+    EXTI->PR |= EXTI_PR_PR0;
+
+    // Enable the interrupt line at NVIC
+    uint32_t prioritygroup = NVIC_GetPriorityGrouping();
+    NVIC_SetPriority(EXTI0_IRQn, NVIC_EncodePriority(prioritygroup, 10, 0));
+    NVIC_EnableIRQ(EXTI0_IRQn);
+}
+
 int main(void)
 {
     // Enable clock for GPIOC peripheral
-    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+    RCC->APB2ENR |= (RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_AFIOEN);
 
     // Configure GPIO pin as output
     GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);  // Clear configuration
     GPIOC->CRH |= GPIO_CRH_MODE13_0;  // Set pin mode to general purpose output (max speed 10 MHz)
 
+    setup_exti0_interrupt();
+
     while (1) {
-        TOGGLE_LED();
-        delay(500);
     }
 }
 
