1cc8bbe46137f00739bcb08ca0d5f292c69cb754
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-05-15 06:48:18 +0530

    ADC

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -53,16 +53,50 @@ void turn_led_on(LedState_t state) {
   */
 int main(void)
 {
-    // Enable clock for GPIOC peripheral
+    uint16_t adcDelay = 0xFFF;
+
+    // Enable clock for GPIOC & ADC1 peripheral
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
 
     // Configure GPIO pin as output
     GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);  // Clear configuration
     GPIOC->CRH |= GPIO_CRH_MODE13_0;  // Set pin mode to general purpose output (max speed 10 MHz)
 
+    // GPIOA1 analog input mode
+    GPIOA->CRL &= ~(GPIO_CRL_CNF1 | GPIO_CRL_MODE1);
+
+    // Caliberate ADC after each power up
+    ADC1->CR2 |= ADC_CR2_ADON;
+    for(int i = 0; i < 3; i++);
+    ADC1->CR2 |= ADC_CR2_CAL;
+    while(ADC1->CR2 & ADC_CR2_CAL);
+    ADC1->CR2 &= ~(ADC_CR2_ADON);
+    // Enable scan mode
+    // ADC->CR1 |= ADC_CR1_SCAN;
+
+    // Continuos mode
+    // ADC->CR2 |= ADC_CR2_CONT;
+
+    // Right alignment
+    // ADC->CR2 &= ~(ADC_CR2_ALIGN);
+
+    // ADC channel 0
+    ADC1->SQR3 = ADC_SQR3_SQ2;
+
+    // Enable ADC
+    ADC1->CR2 |= ADC_CR2_ADON;
+
+    // Start Regular Conversion
+    ADC1->CR2 |= ADC_CR2_SWSTART;
+
+    while(ADC1->SR & ADC_SR_EOC);
+    adcDelay = ADC1->DR;
+
     while (1) {
         TOGGLE_LED();
-        delay(500);
+        delay(adcDelay);
     }
 }
 
