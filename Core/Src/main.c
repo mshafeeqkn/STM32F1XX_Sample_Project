a2edefe3ac68e10046a819a4eeac8c7394ca4bed
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-05-20 07:58:04 +0530

    Added exercise 1 - Adjust dutycycle and frequency using 2 ADC channel in polling method

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -38,81 +38,6 @@ typedef enum {
     TURN_TOGGLE
 } LedState_t;
 
-/**
- * @brief Send a byte through the UART1
- *
- * @param byte to be sent
- */
-void uart1_send_byte(uint8_t ch) {
-    while((USART1->SR & USART_SR_TXE) == 0) {}
-    USART1->DR = ch;
-}
-
-/**
- * @brief Send formatted string through UART1
- *
- * @param format - formatted string
- */
-void uart1_send_string(const char *format, ...) {
-    va_list args;
-    char buffer[128];
-    size_t i, len;
-
-    va_start(args, format);
-    vsprintf(buffer, format, args);
-    va_end(args);
-
-    len = strlen(buffer);
-    for(i = 0; i < len; i++) {
-        uart1_send_byte(buffer[i]);
-    }
-
-    // Wait until transmission complted
-    while((USART1->SR & USART_SR_TC) == 0) {}
-}
-
-/**
- * @brief Setup the UART1 for transmit or recieve
- *
- * @param uart_mode - UART_TX_ENABLE, UART_RX_ENABLE
- *                    or both
- */
-void uart1_setup(uint8_t uart_mode) {
-    uint16_t uart1_cr1_flags = 0;
-
-    // Enable clock to USART1, Alternate function IO and GPIOA
-    RCC->APB2ENR |= (RCC_APB2ENR_USART1EN | RCC_APB2ENR_IOPAEN);
-
-    // Baud rate 2400 @ 8MHz clock frequency
-    USART1->BRR = 0xD05;
-    if(0 != uart_mode) {
-        // Global UART enable if either Tx or Rx enabled.
-        uart1_cr1_flags |= USART_CR1_UE;
-
-        // Enable Transmit mode if required
-        if(uart_mode & UART_TX_ENABLE) {
-            uart1_cr1_flags |= USART_CR1_TE;
-
-            // Configure PA9 as output; 10MHz max; push pull
-            GPIOA->CRH &= ~(GPIO_CRH_CNF9 | GPIO_CRH_MODE9);
-            GPIOA->CRH |= (GPIO_CRH_MODE9_0 | GPIO_CRH_CNF9_1);
-        }
-
-        // Enable Receive mode if required
-        if(uart_mode & UART_RX_ENABLE) {
-            uart1_cr1_flags |= USART_CR1_RE;
-
-            // Configure PA10 input pull-up/pull-down
-            GPIOA->CRH &= ~(GPIO_CRH_CNF10 | GPIO_CRH_MODE10);
-            GPIOA->CRH |= GPIO_CRH_CNF10_1;
-        }
-
-    }
-
-    // Enable UART and required mode
-    USART1->CR1 |= uart1_cr1_flags;
-}
-
 void delay(uint32_t ms) {
     // Simple delay function (not accurate, just for demonstration)
     for (volatile uint32_t i = 0; i < ms * 1000; ++i) {
@@ -139,9 +64,10 @@ int main(void)
 {
     uint16_t adcDelay1 = 0xFFF;
     uint16_t adcDelay2 = 0xFFF;
-    uint16_t adcDelay3 = 0xFFF;
 
-    uart1_setup(UART_TX_ENABLE);
+    uint16_t onTime = 0;
+    uint16_t offTime = 0;
+    uint8_t dutyCycle = 0;
 
     // Enable clock for GPIOA, GPIOC & ADC1 peripheral
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
@@ -160,7 +86,7 @@ int main(void)
     // ADC_CR2 register. When the ADON bit is set for the first
     // time, it wakes up the ADC from Power Down mode. Conversion
     // starts when ADON bit is set for a second time by software
-    // after ADC power-up time
+    // after ADC power-up time (giving ~100ms)
     ADC1->CR2 |= ADC_CR2_ADON;
     delay(100);
     ADC1->CR2 |= ADC_CR2_ADON;
@@ -185,13 +111,22 @@ int main(void)
         while(!(ADC1->SR & ADC_SR_EOC));
         adcDelay2 = ADC1->DR;
 
-        // Get ADC value of channel 3
-        ADC1->SQR3 = 3;
-        ADC1->CR2 |= ADC_CR2_ADON;
-        while(!(ADC1->SR & ADC_SR_EOC));
-        adcDelay3 = ADC1->DR;
+        dutyCycle = adcDelay1 / 36;
 
-        uart1_send_string("ADC1 value = %d;%d;%d\r\n", adcDelay1, adcDelay2, adcDelay3);
+        // Work-around to keep dutycycle not more than 100%
+        if(dutyCycle > 100) {
+            dutyCycle = 100;
+        }
+
+        // Calculate the LED on and off time
+        onTime = (adcDelay2 * dutyCycle) / 100;
+        offTime = adcDelay2 - onTime;
+
+        // Toggle
+        TOGGLE_LED();
+        delay(onTime);
+        TOGGLE_LED();
+        delay(offTime);
     }
 }
 
