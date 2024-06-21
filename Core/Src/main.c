feb9de2cbcf3c8955acf9a507ca785a9844366bf
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-06-21 21:32:17 +0530

    1 sec delay using 8MHz external crystal

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,24 +19,25 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+// Macro defines
 #define TURN_ON_LED()            turn_led_on(TURN_ON)
 #define TURN_OFF_LED()           turn_led_on(TURN_OFF)
 #define TOGGLE_LED()             turn_led_on(TURN_TOGGLE)
 
-
+/**
+ * Enumerations
+ */
 typedef enum {
     TURN_OFF,
     TURN_ON,
     TURN_TOGGLE
 } LedState_t;
 
-void delay(uint32_t ms) {
-    // Simple delay function (not accurate, just for demonstration)
-    for (volatile uint32_t i = 0; i < ms * 1000; ++i) {
-        __NOP();  // No operation (compiler barrier)
-    }
-}
-
+/**
+ * @brief Trun the built in LED on/off/toggle
+ *
+ * @param state - TURN_ON / TURN_OFF / TURN_TOGGLE
+ */
 void turn_led_on(LedState_t state) {
     if(state == TURN_TOGGLE){
         GPIOC->ODR ^= GPIO_ODR_ODR13;
@@ -47,10 +48,44 @@ void turn_led_on(LedState_t state) {
     }
 }
 
+/**
+ * @brief Configure the system clock as 8MHz using
+ * external crystal oscillator.
+ */
+void config_sys_clock() {
+    // Enable HSE (High-Speed External) oscillator
+    RCC->CR |= RCC_CR_HSEON;
+    while ((RCC->CR & RCC_CR_HSERDY) == 0);  // Wait for HSE to be ready
+
+    // Select HSE as the system clock source
+    RCC->CFGR &= ~RCC_CFGR_SW;  // Clear SW bits
+    RCC->CFGR |= RCC_CFGR_SW_HSE;  // Set SW bits to select HSE as system clock
+
+    // Wait until HSE is used as the system clock source
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSE);
+}
+
+/**
+ * @brief Setup the TIM1 for 1 min delay
+ */
 void setup_timer_1() {
+    // Enable system clock to TIM1
     RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
-    TIM1->PSC = 0xC7;
-    TIM1->ARR = 0x9C40;
+
+    // Set pre-scalar 8000; so each count will take
+    // 1ms delay since we are using 8MHz clock
+    TIM1->PSC = 7999;
+
+    // Count 0 - 59999 (both inclusive - so total 60000)
+    TIM1->ARR = 59999;
+
+    // Clear the counter register as 0
+    TIM1->CNT = 0;
+
+    // Clear the overflow flag
+    TIM1->SR &= ~(TIM_SR_UIF);
+
+    // Enable the timer.
     TIM1->CR1 |= TIM_CR1_CEN;
 }
 
@@ -59,6 +94,8 @@ void setup_timer_1() {
   * @retval int
   */
 int main(void) {
+    config_sys_clock();
+
     // Enable clock for GPIOC peripheral
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 
@@ -69,7 +106,8 @@ int main(void) {
     setup_timer_1();
 
     while(1) {
-        while((TIM1->SR & TIM_SR_UIF) == 0) {}
+        // Wait until the overflow flag is set.
+        while( (TIM1->SR & TIM_SR_UIF) == 0) {}
         TIM1->SR &= ~(TIM_SR_UIF);
         TOGGLE_LED();
     }
