31f25e7988f335a57da787f8c006d0ecbe0d842f
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-07-26 14:59:43 +0530

    Empty project

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,26 +1,39 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "i2c_master.h"
+#include "main.h"
 
 // Macro defines
-#define CHIP_ADDR                0x50
+#define TURN_ON_LED()            turn_led_on(TURN_ON)
+#define TURN_OFF_LED()           turn_led_on(TURN_OFF)
+#define TOGGLE_LED()             turn_led_on(TURN_TOGGLE)
+
+/**
+ * Enumerations
+ */
+typedef enum {
+    TURN_OFF,
+    TURN_ON,
+    TURN_TOGGLE
+} LedState_t;
+
+void delay(uint32_t tick) {
+    for (volatile uint32_t i = 0; i < tick; ++i) {
+        __NOP();  // No operation (compiler barrier)
+    }
+}
+
+/**
+ * @brief Trun the built in LED on/off/toggle
+ *
+ * @param state - TURN_ON / TURN_OFF / TURN_TOGGLE
+ */
+void turn_led_on(LedState_t state) {
+    if(state == TURN_TOGGLE){
+        GPIOC->ODR ^= GPIO_ODR_ODR13;
+    } else if(state == TURN_ON) {
+        GPIOC->ODR &= ~(GPIO_ODR_ODR13);
+    } else {
+        GPIOC->ODR |= GPIO_ODR_ODR13;
+    }
+}
 
 /**
  * @brief Configure the system clock as 8MHz using
@@ -39,23 +52,16 @@ void config_sys_clock() {
     while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSE);
 }
 
-
 int main(void) {
-    uint8_t data[20];
     config_sys_clock();
 
-    i2c_init();
-#if 0
-    write_eeprom_data(CHIP_ADDR, 0x00, (uint8_t*)"This is a very long string "
-    "used to test the full memory size of the EEPROM which has the memory size"
-    " of 512 byte of memory but I am not sure how it works and how to fix this"
-    " again some dummy data to fill the maximum length of the full memory address"
-    " right? ok bye again a plenty of data to test the second part of the memory"
-    " which is automatically updating or not we need to test. This is the aim of"
-    " the whole program. Next part is we wil write a protram to read the data"
-    " from the eeprom but why this is not andyyyyy", 512);
-#endif
-    read_eeprom_data(CHIP_ADDR, 0x00, data, 20);
+    // Enable clock for GPIOC peripheral
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+    // Configure GPIO pin as output
+    GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);  // Clear configuration
+    GPIOC->CRH |= GPIO_CRH_MODE13_0;  // Set pin mode to general purpose output (max speed 10 MHz)
+    TURN_OFF_LED();
     while(1) {
     }
 }
